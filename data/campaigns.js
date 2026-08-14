1347af98b1bb6adbc6fb82a1f2d8c11f6782609a
trangttq1105 <trangttq1105@gmail.com>
2026-08-14 21:43:11 +0700

    Create campaigns.js

---
+++ data/campaigns.js
@@ -0,0 +1,120 @@
+const campaigns = {
+
+    trailer: {
+        type: "trailer",
+
+        title: "The Invisible Dragon Official Trailer",
+
+        startTime: "2026-08-14T16:00:00+07:00",
+
+        hashtag: "#TheInvisibleDragon",
+
+        trendingLink:
+            "https://fktrendhouse.carrd.co/#one",
+
+        streaming: [
+            {
+                name: "YouTube",
+                url: "https://www.youtube.com/watch?v=vM6E73hy9-8",
+                current: 400000,
+                target: 500000,
+                instruction: "Watch + Like + Share"
+            },
+
+            {
+                name: "X",
+                url: "https://x.com/GMMTV/status/1993291330819440690",
+                current: 1000000,
+                target: 500000,
+                instruction: "Like + Repost + Reply + Quote"
+            },
+
+            {
+                name: "Instagram",
+                url: "https://www.instagram.com/p/DRep6pak7IX/",
+                current: 2000000,
+                target: 2000000,
+                instruction: "Like + Repost + Share"
+            },
+
+            {
+                name: "TikTok",
+                url: "https://www.tiktok.com/@gmmtvofficial/video/7576668411497942280",
+                current: 300000,
+                target: 500000,
+                instruction: "Like + Repost + Share"
+            },
+
+            {
+                name: "Facebook",
+                url: "https://www.facebook.com/",
+                current: 200000,
+                target: 300000,
+                instruction: "Like + Share"
+            }
+        ],
+
+        trending: {
+            current: 150000,
+            target: 200000,
+            postLink:
+                "https://x.com/intent/tweet?text=%0A%0A%23TheInvisibleDragon"
+        }
+    },
+
+
+    specialEP: {
+
+        type: "ep",
+
+        title: "The Invisible Dragon Special EP",
+
+        startTime: "2026-08-14T16:00:00+07:00",
+
+        hashtag: "#TheInvisibleDragon",
+
+        trendingLink:
+            "https://fktrendhouse.carrd.co/#one",
+
+        clickToPost:
+            "https://x.com/intent/tweet?text=%0A%0A%23TheInvisibleDragon",
+
+        trendingList:
+            "https://x.com/FirstKhaoFans",
+
+        parts: [
+            {
+                name: "Part 1/4",
+                url: "https://www.youtube.com/",
+                instruction: "Watch + Like + Share + Comment"
+            },
+
+            {
+                name: "Part 2/4",
+                url: "https://www.youtube.com/",
+                instruction: "Watch + Like + Share + Comment"
+            },
+
+            {
+                name: "Part 3/4",
+                url: "https://www.youtube.com/",
+                instruction: "Watch + Like + Share + Comment"
+            },
+
+            {
+                name: "Part 4/4",
+                url: "https://www.youtube.com/",
+                instruction: "Watch + Like + Share + Comment"
+            }
+        ],
+
+        tracking: {
+            location: 40,
+            numberOneLocation: 25
+        },
+
+        endMessage:
+            "Thank you for your hard work, see you next week"
+    }
+
+};
